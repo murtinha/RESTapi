5f39e243295e8c7be7397ec053b03fb51752af56
Eric Murta <ericmurta@hotmail.com>
2017-04-18 20:08:31 -0300

    fixed requiments.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/eric/Downloads/RestAPI/venv"
+set -gx VIRTUAL_ENV "/home/eric/Desktop/RestAPI/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
